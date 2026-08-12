c680caa713730d376932c32ba46140cf24d9fc06
y2ahhh <yeaheunlee1190@gmail.com>
2026-08-12 20:49:41 +0900

    Initial Commit

---
+++ src/main/java/com/codeit/sb13/monew/Sb13MonewTeam04Application.java
@@ -0,0 +1,13 @@
+package com.codeit.sb13.monew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb13MonewTeam04Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb13MonewTeam04Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeit/sb13/monew/Sb13MonewTeam04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeit.sb13.monew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb13MonewTeam04ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
