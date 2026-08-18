a5dfa51ffe0971d95f614c4614a1222433b0efed
jangjunseo518-collab <wkdwnstj00@naver.com>
2026-08-18 14:30:07 +0900

    feat: MID4-79 회원가입 응답 DTO 및 매퍼 적용

---
+++ src/main/java/com/codeit/sb13/monew/user/controller/dto/UserCreateResponse.java
@@ -0,0 +1,14 @@
+package com.codeit.sb13.monew.user.controller.dto.response;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record UserCreateResponse(
+    UUID userId,
+    String email,
+    String nickname,
+    LocalDateTime createdAt,
+    LocalDateTime updatedAt
+) {
+
+}
