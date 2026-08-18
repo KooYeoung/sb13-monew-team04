0e1c9e8535a444d6f8a660f69114d4696ac37e11
y2ahhh <yeaheunlee1190@gmail.com>
2026-08-18 16:55:59 +0900

    test: 생성 팩토리의 다른 필드들 검증

--- src/test/java/com/codeit/sb13/monew/notification/domain/NotificationTest.java
+++ src/test/java/com/codeit/sb13/monew/notification/domain/NotificationTest.java
@@ -13,13 +13,17 @@ class NotificationTest {
     @DisplayName("알림을 생성하면 confirmed는 기본값으로 false로 설정된다.")
     void 알림_생성() {
         // given
-        UUID user = UUID.randomUUID();
+        UUID userId = UUID.randomUUID();
         UUID resourceId = UUID.randomUUID();
 
         // when
-        Notification notification = Notification.create(user, "유저 생성 테스트", resourceId, ResourceType.COMMENT);
+        Notification notification = Notification.create(userId, "유저 생성 테스트", resourceId, ResourceType.COMMENT);
 
         // then
         assertThat(notification.isConfirmed()).isFalse();
+        assertThat(notification.getUserId()).isEqualTo(userId);
+        assertThat(notification.getContent()).isEqualTo("유저 생성 테스트");
+        assertThat(notification.getResourceType()).isEqualTo(ResourceType.COMMENT);
+        assertThat(notification.getResourceId()).isEqualTo(resourceId);
     }
 }
