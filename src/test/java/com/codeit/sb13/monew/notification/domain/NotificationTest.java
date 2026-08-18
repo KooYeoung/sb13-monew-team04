aa44f435fba94b907182f25d7b28ecc378a55ed8
y2ahhh <yeaheunlee1190@gmail.com>
2026-08-18 16:33:03 +0900

    chore: User 연관관계 제거 - User 엔티티가 develop에 머지된 후에 다시 작업

--- src/test/java/com/codeit/sb13/monew/notification/domain/NotificationTest.java
+++ src/test/java/com/codeit/sb13/monew/notification/domain/NotificationTest.java
@@ -1,26 +1,20 @@
 package com.codeit.sb13.monew.notification.domain;
 
-import com.codeit.sb13.monew.user.domain.User;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import java.util.UUID;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 class NotificationTest {
     
-    @Test
-    @DisplayName("알림을 생성하면 confirmed는 기본값으로 false로 설정된다.")
-    void 알림_생성() {
-        // given
-        User user = new User();
-        UUID resourceId = UUID.randomUUID();
-    
-        // when
-        Notification notification = Notification.create(user, "유저 생성 테스트", resourceId, ResourceType.COMMENT);
-    
-        // then
-        assertThat(notification.isConfirmed()).isFalse();
-    }
+//    @Test
+//    @DisplayName("알림을 생성하면 confirmed는 기본값으로 false로 설정된다.")
+//    void 알림_생성() {
+//        // given
+//        User user = new User();
+//        UUID resourceId = UUID.randomUUID();
+//
+//        // when
+//        Notification notification = Notification.create(user, "유저 생성 테스트", resourceId, ResourceType.COMMENT);
+//
+//        // then
+//        assertThat(notification.isConfirmed()).isFalse();
+//    }
 }
