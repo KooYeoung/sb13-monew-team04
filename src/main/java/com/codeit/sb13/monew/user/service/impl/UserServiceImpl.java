7c1554da79adbea0da8d2f979b4337dd309c2cfb
jangjunseo518-collab <wkdwnstj00@naver.com>
2026-08-18 14:04:29 +0900

    feat: MID4-79 비밀번호 암호화 및 회원가입 저장 로직 구현

--- src/main/java/com/codeit/sb13/monew/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/codeit/sb13/monew/user/service/impl/UserServiceImpl.java
@@ -1,15 +1,18 @@
 package com.codeit.sb13.monew.user.service.impl;
 
 import com.codeit.sb13.monew.user.controller.dto.UserCreateRequest;
+import com.codeit.sb13.monew.user.domain.User;
 import com.codeit.sb13.monew.user.exception.DuplicateEmailException;
 import com.codeit.sb13.monew.user.repository.UserRepository;
 import com.codeit.sb13.monew.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
   private final UserRepository userRepository;
+  private final PasswordEncoder passwordEncoder;
 
 
   public void signUp(UserCreateRequest request) {
@@ -18,5 +21,15 @@ public class UserServiceImpl implements UserService {
     if (emailExists) {
       throw new DuplicateEmailException(request.email());
     }
+
+    String encode = passwordEncoder.encode(request.password());
+
+    User user = User.builder()
+        .email(request.email())
+        .nickname(request.nickname())
+        .password(encode)
+        .build();
+
+    userRepository.save(user);
   }
 }
--- src/test/java/com/codeit/sb13/monew/user/service/impl/UserServiceImplTest.java
+++ src/test/java/com/codeit/sb13/monew/user/service/impl/UserServiceImplTest.java
@@ -1,6 +1,7 @@
 package com.codeit.sb13.monew.user.service.impl;
 
 import com.codeit.sb13.monew.user.controller.dto.UserCreateRequest;
+import com.codeit.sb13.monew.user.domain.User;
 import com.codeit.sb13.monew.user.exception.DuplicateEmailException;
 import com.codeit.sb13.monew.user.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -9,6 +10,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import static org.mockito.Mockito.verify;
+import static org.mockito.ArgumentMatchers.any;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
@@ -18,6 +22,8 @@ public class UserServiceImplTest {
   
   @Mock
   UserRepository userRepository;
+  @Mock
+  PasswordEncoder passwordEncoder;
 
   @InjectMocks
   UserServiceImpl basicUserService;
@@ -37,7 +43,28 @@ public class UserServiceImplTest {
     // when & then
     assertThatThrownBy(() -> basicUserService.signUp(request))
         .isInstanceOf(DuplicateEmailException.class);
-    
+  }
+
+  @Test
+  @DisplayName("정상적인 요철시에 사용자가 생성된다.")
+  void 정상_값으로_요청_시_사용자가_생성된다() {
+    // given
+    UserCreateRequest request = new UserCreateRequest(
+        "email@example.com",
+        "닉네임",
+        "PassWord123!"
+    );
+    when(userRepository.existsByEmail(request.email()))
+        .thenReturn(false);
+    when(passwordEncoder.encode(request.password()))
+        .thenReturn("encodedPassword123");
+
+    // when
+    basicUserService.signUp(request);
+
+    // then
+    verify(userRepository).save(any(User.class));
+
 
   }
   
