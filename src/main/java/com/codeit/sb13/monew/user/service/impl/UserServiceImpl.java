391655fb3177904df03ec276180223997303d2be
애니프사희망 <jangjunseo518@gmail.com>
2026-08-20 16:16:11 +0900

    feat: MID4-84 닉네임 수정 API 구현 (#24)
    * fix: MID4-84 User 엔티티 UNIQUE 제약을 @Table 방식으로 변경
    
    * feat: MID4-84 닉네임 수정 서비스 로직 및 테스트 구현
    
    * test: MID4-84 닉네임 수정 Controller 및 테스트 구현
    
    * test: MID4-84 닉네임 수정 서비스 로직 및 테스트 구현
    
    * fix: MID4-84 saveAndFlush 결과를 사용해 최신 updatedAt 반영
    
    * test: MID4-84 saveAndFlush 호출 검증 추가
    
    * test: MID4-84 존재하지 않는 userId 시 saveAndFlush 미호출 검증 추가
    
    ---------
    
    Co-authored-by: jangjunseo518-collab <wkdwnstj00@naver.com>

--- src/main/java/com/codeit/sb13/monew/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/codeit/sb13/monew/user/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.codeit.sb13.monew.user.service.impl;
 
+import com.codeit.sb13.monew.global.exception.user.UserNotFoundException;
 import com.codeit.sb13.monew.user.domain.User;
 import com.codeit.sb13.monew.user.exception.DuplicateEmailException;
 import com.codeit.sb13.monew.user.exception.InvalidPasswordException;
@@ -11,6 +12,8 @@ import com.codeit.sb13.monew.user.service.dto.UserCreateCommand;
 import com.codeit.sb13.monew.user.service.dto.UserCreateResult;
 import com.codeit.sb13.monew.user.service.dto.UserLoginCommand;
 import com.codeit.sb13.monew.user.service.dto.UserLoginResult;
+import com.codeit.sb13.monew.user.service.dto.UserUpdateNicknameCommand;
+import com.codeit.sb13.monew.user.service.dto.UserUpdateNicknameResult;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -65,6 +68,16 @@ public class UserServiceImpl implements UserService {
       throw new InvalidPasswordException(command.email());
     }
     return userMapper.toLoginResult(user);
+  }
+
+  @Override
+  @Transactional
+  public UserUpdateNicknameResult updateNickname(UserUpdateNicknameCommand command) {
+    User user = userRepository.findById(command.userId())
+        .orElseThrow(() -> new UserNotFoundException(command.userId()));
+    user.updateNickname(command.nickname());
+    User saveUser = userRepository.saveAndFlush(user);
+    return userMapper.toUpdateNicknameResult(saveUser);
   }
 
   private boolean isEmailUniqueViolation(DataIntegrityViolationException e) {
