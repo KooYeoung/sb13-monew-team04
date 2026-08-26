d3ead7760c5610e0273d99cf584ec022099d52d3
애니프사희망 <jangjunseo518@gmail.com>
2026-08-26 14:27:56 +0900

    MID4-181 자동 삭제 배치 안정성 개선 [배치 대상 재검증] (#82)
    * fix: MID4-181 배치 대상 재검증 로직 및 테스트 구현
    
    * refactor: MID4-181 findById 중복 호출 제거 및 트랜잭션 일관성 확보.
    
    ---------
    
    Co-authored-by: jangjunseo518-collab <wkdwnstj00@naver.com>

--- src/main/java/com/codeit/sb13/monew/user/service/UserHardDeleteExecutor.java
+++ src/main/java/com/codeit/sb13/monew/user/service/UserHardDeleteExecutor.java
@@ -6,7 +6,9 @@ import com.codeit.sb13.monew.comment.repository.CommentRepository;
 import com.codeit.sb13.monew.global.exception.user.UserNotFoundException;
 import com.codeit.sb13.monew.interest.repository.SubscribeRepository;
 import com.codeit.sb13.monew.notification.repository.NotificationRepository;
+import com.codeit.sb13.monew.user.domain.User;
 import com.codeit.sb13.monew.user.repository.UserRepository;
+import java.time.LocalDateTime;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -27,6 +29,10 @@ public class UserHardDeleteExecutor {
   public void hardDeleteUser(UUID userId) {
     userRepository.findById(userId)
         .orElseThrow(() -> new UserNotFoundException(userId));
+    deleteAllRelateDataData(userId);
+  }
+
+  private void deleteAllRelateDataData(UUID userId) {
     // FK 제약 순서 고려
     // CommentLike→ Comment → ArticleView → Subscribe → Notification → User
     commentLikeRepository.deleteByComment_User_Id(userId);
@@ -36,6 +42,17 @@ public class UserHardDeleteExecutor {
     subscribeRepository.deleteByUserId(userId);
     notificationRepository.deleteByUser_Id(userId);
     userRepository.deleteById(userId);
+
+  }
+
+  @Transactional(propagation = Propagation.REQUIRES_NEW)
+  public void hardDeleteExpiredUser(UUID userId, LocalDateTime threshold) {
+    User user = userRepository.findById(userId)
+        .orElseThrow(() -> new UserNotFoundException(userId));
+    LocalDateTime deletedAt = user.getDeletedAt();
+    if(deletedAt != null &&  deletedAt.isBefore(threshold)) {
+      deleteAllRelateDataData(userId);
+    }
   }
 
 }
